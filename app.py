6cdacdb4c61e796d60dda8fbe92ba0e5e77cda50
Rachael Ferguson <ferguson.rachael@outlook.com>
2020-11-25 00:08:52 -0500

    Add endpoints for analysis and exploration.

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, request
 import zipfile
 
-from helpers.data_read_write import flattened_exercise_to_csv
+from helpers.data_read_write import flattened_exercise_to_csv, flattened_exercise, csv_to_dataframe
+from helpers.model import SavedCSV
+from helpers.paths import SAVED_CSV_PATH, SAVED_DATA_MAP
 
 app = Flask(__name__, template_folder='view')
 
+TMESTAMPED_SQLITE_DATA = csv_to_dataframe(SavedCSV(csv=SAVED_CSV_PATH, data_map=SAVED_DATA_MAP))
+
 @app.route('/')
 def home():
-    """Print 'Hello, world!' as the response body."""
+    """main page."""
+
     return render_template('home.html')
+
+@app.route('/paginated-data')
+def paginated_data():
+    """Return sqlite flattend data as a series of pages."""
+    # Set the pagination configuration
+    page = request.args.get('page', 1, type=int)
+
+    num_entriees_per_page = 10
+    paged_data = TMESTAMPED_SQLITE_DATA[(page-1)*num_entriees_per_page: num_entriees_per_page*page]
+    return render_template('paginated_data.html', sqlite_data=paged_data)
+
 
 @app.route('/download_csv')
 def download_csv():
